5ac29a4cffb93cabb4ee4f5198442efd62113be4
paul <pbranz@gmail.com>
2021-09-14 18:15:56 +0100

    telephony

--- adafruit_hid/telephony_control.py
+++ adafruit_hid/telephony_control.py
@@ -21,20 +21,19 @@ import struct
 import time
 from . import find_device
 
-
 class TelephonyControl:
     """Send TelephonyControl code reports, used by headsets."""
 
     def __init__(self, devices):
-        """Create a ConsumerControl object that will send Consumer Control Device HID reports.
+        """Create a TelephonyControl object that will send Telephony Control Device HID reports.
 
-        Devices can be a list of devices that includes a Consumer Control device or a CC device
+        Devices can be a list of devices that includes a Telephony Control device or a CC device
         itself. A device is any object that implements ``send_report()``, ``usage_page`` and
         ``usage``.
         """
-        self._consumer_device = find_device(devices, usage_page=0x0C, usage=0x01)
+        self._telephony_device = find_device(devices, usage_page=0x0B, usage=0x05)
 
-        # Reuse this bytearray to send consumer reports.
+        # Reuse this bytearray to send telephony reports.
         self._report = bytearray(2)
 
         # Do a no-op to test if HID device is ready.
@@ -45,56 +44,56 @@ class TelephonyControl:
             time.sleep(1)
             self.send(0x0)
 
-    def send(self, consumer_code):
-        """Send a report to do the specified consumer control action,
+    def send(self, telephony_code):
+        """Send a report to do the specified telephony control action,
         and then stop the action (so it will not repeat).
 
-        :param consumer_code: a 16-bit consumer control code.
+        :param telephony_code: a 16-bit telephony control code.
 
         Examples::
 
-            from adafruit_hid.consumer_control_code import ConsumerControlCode
+            from adafruit_hid.telephony_control_code import TelephonyControlCode
 
             # Raise volume.
-            consumer_control.send(ConsumerControlCode.VOLUME_INCREMENT)
+            telephony_control.send(TelephonyControlCode.VOLUME_INCREMENT)
 
             # Advance to next track (song).
-            consumer_control.send(ConsumerControlCode.SCAN_NEXT_TRACK)
+            telephony_control.send(TelephonyControlCode.SCAN_NEXT_TRACK)
         """
-        self.press(consumer_code)
+        self.press(telephony_code)
         self.release()
 
-    def press(self, consumer_code):
+    def press(self, telephony_code):
         """Send a report to indicate that the given key has been pressed.
-        Only one consumer control action can be pressed at a time, so any one
+        Only one telephony control action can be pressed at a time, so any one
         that was previously pressed will be released.
 
-        :param consumer_code: a 16-bit consumer control code.
+        :param telephony_code: a 16-bit telephony control code.
 
         Examples::
 
-            from adafruit_hid.consumer_control_code import ConsumerControlCode
+            from adafruit_hid.telephony_control_code import TelephonyControlCode
 
             # Raise volume for 0.5 seconds
-            consumer_control.press(ConsumerControlCode.VOLUME_INCREMENT)
+            telephony_control.press(TelephonyControlCode.VOLUME_INCREMENT)
             time.sleep(0.5)
-            consumer_control.release()
+            telephony_control.release()
         """
-        struct.pack_into("<H", self._report, 0, consumer_code)
-        self._consumer_device.send_report(self._report)
+        struct.pack_into("<H", self._report, 0, telephony_code)
+        self._telephony_device.send_report(self._report)
 
     def release(self):
-        """Send a report indicating that the consumer control key has been
-        released. Only one consumer control key can be pressed at a time.
+        """Send a report indicating that the telephony control key has been
+        released. Only one telephony control key can be pressed at a time.
 
         Examples::
 
-            from adafruit_hid.consumer_control_code import ConsumerControlCode
+            from adafruit_hid.telephony_control_code import TelephonyControlCode
 
             # Raise volume for 0.5 seconds
-            consumer_control.press(ConsumerControlCode.VOLUME_INCREMENT)
+            telephony_control.press(TelephonyControlCode.VOLUME_INCREMENT)
             time.sleep(0.5)
-            consumer_control.release()
+            telephony_control.release()
         """
         self._report[0] = self._report[1] = 0x0
-        self._consumer_device.send_report(self._report)
+        self._telephony_device.send_report(self._report)
